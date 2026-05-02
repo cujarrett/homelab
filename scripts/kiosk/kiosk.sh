9700a68253728c3b2561b2d25cfda4e4b6a52d42
Matt jarrett <cujarrett@gmail.com>
2026-05-02 13:37:52 -0500

    fix: update kiosk display URL to point to the correct Grafana playlist

--- scripts/kiosk/kiosk.sh
+++ scripts/kiosk/kiosk.sh
@@ -7,6 +7,6 @@ until curl -sk https://grafana.local.lab/api/health | grep -q '"database": "ok"'
 done
 
 while true; do
-  chromium --noerrdialogs --disable-infobars --kiosk --incognito --disable-translate --disable-features=TranslateUI --ignore-certificate-errors --force-device-scale-factor=1 --window-size=1424,280 --disable-gpu --disable-dev-shm-usage "--js-flags=--max-old-space-size=64" --renderer-process-limit=1 --disable-extensions --disable-background-networking --disable-sync --disable-hang-monitor --disable-component-update --process-per-site "https://grafana.local.lab/playlists/play/admt9pc?kiosk"
+  chromium --noerrdialogs --disable-infobars --kiosk --incognito --disable-translate --disable-features=TranslateUI --ignore-certificate-errors --force-device-scale-factor=1 --window-size=1424,280 --disable-gpu --disable-dev-shm-usage "--js-flags=--max-old-space-size=64" --renderer-process-limit=1 --disable-extensions --disable-background-networking --disable-sync --disable-hang-monitor --disable-component-update --process-per-site "https://grafana.local.lab/playlists/play/bfkvgx130fncwc?kiosk"
   sleep 2
 done
