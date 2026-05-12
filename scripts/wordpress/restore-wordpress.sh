99299c677371f02c01aea633e34ecadf44624ad3
Matt jarrett <cujarrett@gmail.com>
2026-05-12 11:27:37 -0500

    docs: correct capitalization of XWordPressPlatform to XWordpress in restore script

--- scripts/wordpress/restore-wordpress.sh
+++ scripts/wordpress/restore-wordpress.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # restore-wordpress.sh
-# Restores a WordPress site into the XWordPressPlatform stack.
+# Restores a WordPress site into the XWordpress stack.
 #
 # Usage:
 #   ./scripts/restore-wordpress.sh \
